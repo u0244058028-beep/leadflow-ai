2c96863f612bca34bd699b0768f1f4233f6090b6
u0244058028-beep <u0244058028@gmail.com>
2026-02-18 20:20:26 +0100

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,21 @@
 'use client'
 
 import { supabase } from "@/lib/supabase"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function LoginPage() {
 
   const [email, setEmail] = useState("")
+  const router = useRouter()
 
   async function login() {
 
     await supabase.auth.signInWithOtp({
-      email
+      email,
+      options: {
+        emailRedirectTo: window.location.origin + "/auth"
+      }
     })
 
     alert("Check your email for login link")
