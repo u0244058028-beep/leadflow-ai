7d42c04e66e942193431aa755e179fd04496b55c
u0244058028-beep <u0244058028@gmail.com>
2026-02-18 20:21:14 +0100

    Create page.tsx

---
+++ app/auth/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useEffect } from "react"
+import { supabase } from "@/lib/supabase"
+import { useRouter } from "next/navigation"
+
+export default function AuthPage() {
+
+  const router = useRouter()
+
+  useEffect(() => {
+
+    async function handleAuth() {
+
+      await supabase.auth.getSession()
+
+      router.push("/test")
+    }
+
+    handleAuth()
+
+  }, [])
+
+  return <p>Logging you in...</p>
+}
