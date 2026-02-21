9a85e2db256202ac39719d5174d18b9d73a374b0
u0244058028-beep <u0244058028@gmail.com>
2026-02-21 11:04:13 +0100

    Create reports.tsx

---
+++ pages/reports.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout'
+import Reports from '@/components/Reports'
+
+export default function ReportsPage() {
+  return (
+    <Layout>
+      <Reports />
+    </Layout>
+  )
+}
