58f278c6207a8c6238f04348a9b079c6f3080bd6
u0244058028-beep <u0244058028@gmail.com>
2026-02-22 11:42:01 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="no">
-      <Head />
+    <Html lang="en">
+      <Head>
+        {/* Puter.js – gratis AI uten API-nøkler */}
+        <script src="https://js.puter.com/v2/"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
