54bcf79e2a3ee1502aedae8df6119bb8ecbf1859
u0244058028-beep <u0244058028@gmail.com>
2026-02-25 19:01:01 +0100

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-02-24.acacia', // 🟢 Oppdatert til siste API-versjon
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 });
